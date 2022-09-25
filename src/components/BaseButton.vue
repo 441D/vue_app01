894092d1fb740846b8ed6ccdf23e74b2c0c9cda7
441dai <441dai@gmail.com>
2022-09-25 22:32:38 +0900

    ボタンコンポーネント化

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+  const props = defineProps({ color: String });
+  const emit = defineEmits([ 'on-click' ]);
+  const onClick = () => {
+    emit('on-click');
+  };
+</script>
+
+<template>
+  <button class="btn" :class="props.color" @click="onClick"><slot /></button>
+</template>
+
+<style scoped>
+  .btn {
+    padding: 8px;
+    background-image: linear-gradient(-20deg, #616161 0%, #9bc5c3 100%);
+    border-radius: 6px;
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+  }
+  .blue {
+    background: #00A7E1;
+  }
+  .red {
+    background: #EF6461;
+  }
+</style>
